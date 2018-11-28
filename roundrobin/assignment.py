d50b1e80462cf66dc4093d6ac68ef422ddec522c
Mike Chaberski <mchaberski@gmail.com>
2018-11-27 19:30:41 -0500

    render output in table

--- roundrobin/assignment.py
+++ roundrobin/assignment.py
@@ -51,6 +51,30 @@ def tokenize(specs):
     return tuple(specs)
 
 
+class AssignmentOracle(object):
+
+    def __init__(self, assignments):
+        self.assignments = assignments
+    
+    def get_taker(self, giver, slot):
+        return self.get_by_item(giver, slot, _SLOT)
+    
+    def get_slot(self, giver, taker):
+        return self.get_by_item(giver, taker, _TAKER)
+
+    def get_by_item(self, giver, other, idx, absence_fn=None):
+        absence_fn = absence_fn or (lambda x: None)
+        assmts = self.assignments.get(giver, [])
+        for a in assmts:
+            if a[idx] == other:
+                for i in range(len(a)):
+                    if i != idx:
+                        return a[i]
+        absence_fn((other, assmts, idx, giver))
+        return None
+
+
+
 def render_assignments(givers, assignments_by_giver, fmt, ofile=sys.stdout):
     if fmt == 'json':
         pretty = {}
@@ -61,6 +85,21 @@ def render_assignments(givers, assignments_by_giver, fmt, ofile=sys.stdout):
         for giver in givers:
             for a in assignments_by_giver[giver]:
                 print("{} gives a \"{}\" gift to {}".format(giver, a[_SLOT], a[_TAKER]), file=ofile)
+    elif fmt == 'table_takers':
+        # TODO assert that set(givers) == set(takers)
+        takers = givers
+        cells_per_row = 1 + len(givers)
+        divider = ''.join(['-' for i in range((8 + 3) * cells_per_row + 1)])
+        row_fmt = "| " + " | ".join(["%8s" for i in range(cells_per_row)]) + " |"
+        header_row = row_fmt % tuple([""] + list(takers))
+        print(divider, file=ofile)
+        print(header_row, file=ofile)
+        print(divider, file=ofile)
+        oracle = AssignmentOracle(assignments_by_giver)
+        for giver in givers:
+            row_data = tuple([giver] + [oracle.get_slot(giver, taker) or "" for taker in takers])
+            print(row_fmt % row_data)
+            print(divider, file=ofile)
     else:
         _log.info("bad format %s", fmt)
         raise ValueError("format not allowed")
@@ -74,7 +113,7 @@ def main():
     p.add_argument("--takers", help="list of takers; givers are used by default")
     p.add_argument("--slots", nargs='+', default='', help="set gift types")
     p.add_argument("--seed", type=int, help="set random number generator seed")
-    p.add_argument("--format", choices=('json', 'english'), default='json', help="output format")
+    p.add_argument("--format", choices=('json', 'english', 'table_takers'), default='english', help="output format")
     p.add_argument("--log-level", choices=('DEBUG', 'INFO', 'WARN', 'ERROR'), default='INFO', help="set log level")
     args = p.parse_args()
     logging.basicConfig(level=logging.__dict__[args.log_level])
